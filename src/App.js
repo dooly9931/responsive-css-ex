c403ca8fdccb28c9d284a682934f362041540df1
Kyoungseo <kyoungseo@hyperithm.com>
2022-04-14 17:21:26 +0900

    Check RWD with react-responsive, check sass with scss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import MediaQuery from "react-responsive";
+import DesktopFavorites from "./Containers/Favorites/DesktopFavorites";
+import MobileFavorites from "./Containers/Favorites/MobileFavorites";
+import Favorites from "./Containers/Favorites/Favorites";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MediaQuery minWidth={786}>
+        <DesktopFavorites />
+      </MediaQuery>
+      <MediaQuery maxWidth={786}>
+        <MobileFavorites />
+      </MediaQuery>
+      <Favorites />
     </div>
   );
 }
---
+++ src/Containers/Favorites/Favorites.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Favorites.scss";
+
+const Favorites = (props) => {
+  return (
+    <div>
+      <div id="favorites-page">Favorites</div>
+      <button className="button-enable">Enable</button>
+    </div>
+  );
+};
+
+export default Favorites;
